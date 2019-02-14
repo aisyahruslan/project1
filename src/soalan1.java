fe32dfd4b4d63dda98d25112a118ce5aa295c3de
pc07 <pc07@pc07-PC>
2019-02-14 09:10:16 +0800

    commit1

---
+++ src/soalan1.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author pc 09
+ */
+public class soalan1 {
+public static void main(String[] args) {
+
+ int bm,bi,mat,sej,jumlah;
+ double purata, peratus;
+ 
+ 
+ bm=78;
+ bi=53;
+ mat=81;
+ sej=97;
+ 
+ 
+ jumlah= bm+bi+mat+sej;
+ purata=jumlah/4;
+ peratus=jumlah*100/400;
+ 
+ 
+ System.out.println("KEPUTUSAN MARKAH BAGI 4 MATA PELAJARAN");
+ 
+ System.out.println("B.Melayu :"+bm+"\t B.Inggeris :"+bi);
+ System.out.println("Matematik :"+mat+ "\t\t Sejarah :"+sej);
+ System.out.println("Jumlah markah Keseluruhan :"+jumlah);
+ System.out.println("Purata markah Keseluruhan :"+purata);
+ System.out.println("Peratus markah Keseluruhan :"+peratus);
+ }
+}
+
+
+
---
+++ src/soalan2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author pc 09
+ */
+public class soalan2 {
+public static void main(String[] args) {
+final double Pi=3.14159;
+double jejari, luas;
+jejari=20;
+luas=jejari*jejari*Pi;
+System.out.println("Luas  bagi  bulatan  yang  mempunyai  jejari"+jejari+" adalah "+luas+" cm persegi");
+ } 
+}
